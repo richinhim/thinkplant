95fc07b0b922b42946d7b08e75cbd9afa43171c9
kwcba <kwlsy666@naver.com>
2018-02-26 17:25:59 +0900

    1.단계 메뉴구성 2.단계 라우팅 구성
    3.단계 -메인 구성 및 Create Think Plant

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-   <p>Home/Main Page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item>
+            <div class="title">
+              ThinkPlant <br/>
+              첫 번째 관점, 익숙한 것의 낯선 조합, 아이디어 탄생의 비결<br/>
+              두 번째 관점, 개념정의를 바꾸면 아이디어는 저절로 생긴다<br/>
+              세 번째 관점, 가슴으로 생각하는 역지사지(易地思之), 아이디어 잉태의 산실<br/>
+              네 번째 관점, 전제나 가정을 없앨 때, 전대미문의 새로운 아이디어가 부각된다<br/>
+              다섯 번째 관점, 내가 사용하는 단어가 아이디어의 품격을 좌우한다<br/>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
--- src/components/thinks/CreateThinkPlant.vue
+++ src/components/thinks/CreateThinkPlant.vue
@@ -1,5 +1,106 @@
 <template>
-  <div>
-   <p>Create Think Plant Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="primary--text"> Create a new Think Plant</h1>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateThinkPlant">
+          <v-layout row>
+            <v-flex xs4>
+              <v-subheader>Title of Think</v-subheader>
+            </v-flex>
+            <v-flex xs8>
+              <v-text-field
+                name="title"
+                label="Insert a new Title of Think"
+                v-model="title"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs4>
+              <v-subheader>Content</v-subheader>
+            </v-flex>
+            <v-flex xs8>
+              <v-text-field
+                name="content"
+                label="Insert a new Contents of Think"
+                class="input-group--focused"
+                multi-line
+                v-model="content"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs4>
+              <v-subheader>IsPublic</v-subheader>
+            </v-flex>
+            <v-flex xs8>
+              <v-checkbox
+              :label="`공개여부: ${ispublic.toString()}`"
+              v-model="ispublic"
+              >
+              </v-checkbox>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs4>
+              <v-subheader>Tag</v-subheader>
+            </v-flex>
+            <v-flex xs8>
+              <v-text-field
+                name="tag"
+                label="Insert Tag"
+                single-line
+                v-model="tag"
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+              :disabled="!formIsValid"
+              type="submit">Create ThinkPlant
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+ export default {
+   data () {
+     return {
+       title: '',
+       content: '',
+       ispublic: true,
+       tag: ''
+     }
+   },
+   computed: {
+     formIsValid () {
+       return this.title !== '' && this.content !== ''
+     }
+   },
+   methods: {
+     onCreateThinkPlant () {
+       if (!this.formIsValid) {
+         return
+       }
+       const thinkPlantData = {
+         title: this.title,
+         content: this.content,
+         ispublic: this.ispublic,
+         tag: this.tag
+       }
+     }
+   }
+ }
+</script>
